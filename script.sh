1e08c481edae09f7aafaf38d64640956bb451ce8
Popa Sebastian <sebastianpopa208@gmail.com>
2023-02-28 22:47:17 +0200

    script!

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-sudo g++ -c main.cpp && sudo g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system && sudo ./sfml-app
+g++ *.cpp -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system && ./sfml-app

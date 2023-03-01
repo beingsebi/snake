0a649c5b4d1011a2cb3889b8d4b1eddfce8c08c8
Popa Sebastian <sebastianpopa208@gmail.com>
2023-03-01 03:04:13 +0200

    consts

---
+++ constants.cpp
@@ -0,0 +1,12 @@
+#include "constants.hpp"
+#include <SFML/Graphics.hpp>
+
+sf::Vector2f const Constants::cell_size = sf::Vector2f(40.f, 40.f);
+
+sf::Color const Constants::color1 = sf::Color(34, 40, 49, 255);
+sf::Color const Constants::color2 = sf::Color(57, 62, 70, 255);
+sf::Color const Constants::color3 = sf::Color(0, 173, 181, 255);
+sf::Color const Constants::color4 = sf::Color(238, 238, 238, 255);
+
+int const Constants::lines = 20;
+int const Constants::columns = 15;
---
+++ constants.hpp
@@ -0,0 +1,18 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <SFML/Graphics.hpp>
+struct Constants
+{
+    static sf::Vector2f const cell_size;
+
+    static sf::Color const color1;
+    static sf::Color const color2;
+    static sf::Color const color3;
+    static sf::Color const color4;
+
+    static int const lines;
+    static int const columns;
+};
+
+#endif

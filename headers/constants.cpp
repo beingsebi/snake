802d709af5a4a573908fdfe150da483cfbcc7de1
Popa Sebastian <sebastianpopa208@gmail.com>
2023-03-04 00:41:07 +0200

    new colors

--- headers/constants.cpp
+++ headers/constants.cpp
@@ -7,6 +7,8 @@ sf::Color const Constants::color1 = sf::Color(34, 40, 49, 255);
 sf::Color const Constants::color2 = sf::Color(57, 62, 70, 255);
 sf::Color const Constants::color3 = sf::Color(0, 173, 181, 255);
 sf::Color const Constants::color4 = sf::Color(238, 238, 238, 255);
+sf::Color const Constants::color5 = sf::Color(175, 225, 175, 255);
+sf::Color const Constants::color6 = sf::Color(80, 200, 120, 255);
 
 int const Constants::lines = 15;
 int const Constants::columns = 20;

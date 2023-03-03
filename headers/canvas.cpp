59b8f54b6251e796ac70535550d3a35e96c3d074
Popa Sebastian <sebastianpopa208@gmail.com>
2023-03-03 23:48:47 +0200

    bug fix, add cells in disabled_cells

--- headers/canvas.cpp
+++ headers/canvas.cpp
@@ -6,11 +6,18 @@
 Canvas::Canvas()
 {
     std::mt19937 mt(time(nullptr));
-    for (int i = 0; i < Constants::lines; i++)
+    for (int i = 0, ax; i < Constants::lines; i++)
     {
         this->matrix.push_back(vector<Cell>());
+        matrix.back().reserve(Constants::columns);
         for (int j = 0; j < Constants::columns; j++)
-            this->matrix[i].push_back(Cell(i, j,
-                                           (((mt() % 11) != 0) | (i == 0) | (j == 0) | (i == Constants::lines - 1) | (j == Constants::columns - 1))));
+        {
+            ax = (((mt() % 11) != 0) | (i == 0) | (j == 0) | (i == Constants::lines - 1) | (j == Constants::columns - 1));
+            if (ax)
+                this->matrix[i].push_back(Cell(i, j, 1));
+            else
+                this->matrix[i].push_back(Cell(i, j, 0)),
+                    this->disabled_cells.push_back({i, j});
+        }
     }
 }

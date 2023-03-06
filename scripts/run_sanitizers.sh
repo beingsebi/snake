f1fb8fb5446cfbfe269cece876c778596e298c20
Popa Sebastian <sebastianpopa208@gmail.com>
2023-03-06 13:47:17 +0200

    cmake?>??!??!??!

---
+++ scripts/run_sanitizers.sh
@@ -0,0 +1,6 @@
+./"${ZIP_NAME}"/"${EXECUTABLE_NAME}" &
+sleep 3 && xdotool search --name "${APP_WINDOW}" key X &&
+sleep 1 && xdotool search --name "${APP_WINDOW}" key Y &&
+sleep 1 && xdotool search --name "${APP_WINDOW}" key X &&
+sleep 1 && xdotool search --name "${APP_WINDOW}" key Z &&
+sleep 2 && xdotool search --name "${APP_WINDOW}" key Alt+F4

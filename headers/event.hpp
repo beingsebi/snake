98b63ba8260fc0f5153b59334e3b77e5414addfb
Popa Sebastian <sebastianpopa208@gmail.com>
2023-04-13 18:39:17 +0300

    gud; TODO: add constructors;

--- headers/event.hpp
+++ headers/event.hpp
@@ -1,17 +1,37 @@
 #ifndef EVENT_H
 #define EVENT_H
+#include <string>
 
 class event
 {
 public:
+    int posx, posy, scale;
+    std::string path;
+    virtual void actiune() = 0;
 };
 
-class flower : public event
+class fruct : public event
 {
+    int to_add;
+    void actiune() override{};
 };
 
-class fruit : public event
+class floare : public event
 {
+public:
+    static int count;
+    int new_theme;
+    void actiune() override{
+
+    };
+    void bonus(){
+
+    };
+};
+
+class cheie : public event
+{
+    void actiune() override{};
 };
 
 #endif
--- src/event.cpp
+++ src/event.cpp
@@ -1,1 +1,3 @@
 #include "../headers/event.hpp"
+
+int floare::count = 0;

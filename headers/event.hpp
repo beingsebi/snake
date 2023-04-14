9a6f5cfb8393868b580429582a0ec653e9293916
Popa Sebastian <sebastianpopa208@gmail.com>
2023-04-14 16:07:43 +0300

    constructors,destructors events

--- headers/event.hpp
+++ headers/event.hpp
@@ -1,37 +1,49 @@
 #ifndef EVENT_H
 #define EVENT_H
 #include <string>
-
-class event
+using std::pair;
+using std::string;
+class Event
 {
-public:
-    int posx, posy, scale;
-    std::string path;
+protected:
+    pair<int, int> pos;
+    string path;
+    int scale;
+
     virtual void actiune() = 0;
+    Event(const pair<int, int> &, const string &, const int);
+    virtual ~Event() = default;
 };
 
-class fruct : public event
+class Fruit : public Event
 {
     int to_add;
     void actiune() override{};
-};
+    virtual ~Fruit() = default;
 
-class floare : public event
-{
 public:
-    static int count;
-    int new_theme;
-    void actiune() override{
-
-    };
-    void bonus(){
-
-    };
+    Fruit(const pair<int, int> &, const string &, const int, const int);
 };
 
-class cheie : public event
+class Flower : public Event
+{
+    static int count;
+    int theme;
+    void actiune() override;
+    void bonus();
+    virtual ~Flower() = default;
+
+public:
+    Flower(const pair<int, int> &, const string &, const int, const int);
+};
+
+class Key : public Event
 {
     void actiune() override{};
+    virtual ~Key() = default;
+
+public:
+    Key(const pair<int, int> &, const string &, const int);
 };
 
 #endif
--- src/event.cpp
+++ src/event.cpp
@@ -1,3 +1,11 @@
 #include "../headers/event.hpp"
 
-int floare::count = 0;
+int Flower::count = 0;
+
+Event::Event(const pair<int, int> &p, const string &pth, const int scl) : pos{p}, path{pth}, scale{scl} {}
+
+Fruit::Fruit(const pair<int, int> &p, const string &pth, const int scl, const int td) : Event{p, pth, scl}, to_add{td} {}
+
+Flower::Flower(const pair<int, int> &p, const string &pth, const int scl, const int thm) : Event{p, pth, scl}, theme{thm} {}
+
+Key::Key(const pair<int, int> &p, const string &pth, const int scl) : Event{p, pth, scl} {}

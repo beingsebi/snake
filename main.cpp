8d300439d7fa6139f07a17cbd5ea76212a0bc31d
Popa Sebastian <sebastianpopa208@gmail.com>
2023-04-16 20:52:57 +0300

    ccc1

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
     Game game;
     while (game.is_running())
     {
-        game.render(); // maybe better in this order??
+        game.render(); // maybe better to render first and update later in order to reduce lag?
         game.update();
     }
     return 0;

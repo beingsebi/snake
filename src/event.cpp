bc1c55bc4983adf14cd83591f3069ff9ea9f1ce6
Popa Sebastian <sebastianpopa208@gmail.com>
2023-05-04 18:23:53 +0300

    BACK to normal

--- src/event.cpp
+++ src/event.cpp
@@ -45,10 +45,3 @@ void Flower::reset_count()
 {
     count = 0;
 }
-
-Vegetable::Vegetable(Game *g, const pair<int, int> &p, const pair<int, int> &off, const string &pth, const float scl, const int td) : Event{g, p, off, pth, scl}, penalty{td} {}
-
-void Vegetable::actiune()
-{
-    this->pg->add_score(this->penalty * -1);
-}
